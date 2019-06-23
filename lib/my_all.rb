efffe028f721b4dbde04846d59325a0fc4c82016
Lantz Warrick <betalantz@gmail.com>
2019-06-23 07:06:20 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0
+  while i < collection.length
+    if !yield(collection[i])
+      return false
+    else
+      i += 1
+    end
+  end
+  return true
 end
