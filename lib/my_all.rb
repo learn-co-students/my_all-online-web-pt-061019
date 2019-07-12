188a396b937c82098f8d1bfb7a9a7183c4f4057d
Dex Fitch <fitch.dexter@gmail.com>
2019-07-12 12:02:02 -0700

    alls

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
 def my_all?(collection)
+  i = 0
+  return_val = []
+  
+  while i < collection.length
+    return_val << yield(collection[i])
+    i = i + 1
+  end
 
+  return_val.include?(false) ? false : true
 end
